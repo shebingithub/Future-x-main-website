67d8cf0156d4f4657d62e48cdf641e60c5ca6626
shebingithub <shebicphaked99@gmail.com>
2024-01-18 13:07:06 +0530

    first commit

---
+++ virtual/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\shebi\Downloads\mainFUTUREX ECommerceSite-Django (2)\virtual\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
